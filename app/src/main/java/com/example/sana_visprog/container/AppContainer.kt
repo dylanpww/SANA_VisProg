64d91df64c8785cc64ea2722933c3e4239643767
Vanness Aurelius Gunawan <vaurelius03@student.ciputra.ac.id>
2025-12-15 11:39:47 +0700

    container&network_province

--- app/src/main/java/com/example/sana_visprog/container/AppContainer.kt
+++ app/src/main/java/com/example/sana_visprog/container/AppContainer.kt
@@ -2,12 +2,15 @@ package com.example.sana_visprog.container
 
 import android.content.Context
 import com.example.sana_visprog.repository.CategoryRepository
+import com.example.sana_visprog.repository.ProvinceRepository
 import com.example.sana_visprog.service.CategoryService
+import com.example.sana_visprog.service.ProvinceService
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class AppContainer(private val context: Context) {
-    private val baseUrl = "http://192.168.0.9:3000/api/" //URL Vanness
+//    private val baseUrl = "http://192.168.0.9:3000/api/" //URL Vanness Home Wifi
+    private val baseUrl = "http://192.168.0.9:3000/api/" //URL Vanness Data HP
 
     private val retrofit: Retrofit = Retrofit.Builder()
         .baseUrl(baseUrl)
@@ -17,4 +20,8 @@ class AppContainer(private val context: Context) {
     private val categoryService: CategoryService = retrofit.create(CategoryService::class.java)
 
     val categoryRepository: CategoryRepository = CategoryRepository(categoryService)
+
+    private val provinceService: ProvinceService = retrofit.create(ProvinceService::class.java)
+
+    val provinceRepository: ProvinceRepository = ProvinceRepository(provinceService)
 }
