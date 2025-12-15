52ee84170031df3432fe0a88ca1816f910b8285e
ClarrenceAdriano <cadriano@student.ciputra.ac.id>
2025-12-15 21:06:07 +0700

    Register Service

--- app/src/main/java/com/example/sana_visprog/service/AuthService.kt
+++ app/src/main/java/com/example/sana_visprog/service/AuthService.kt
@@ -1,6 +1,7 @@
 package com.example.sana_visprog.service
 
 import com.example.sana_visprog.dto.User.LoginRequest
+import com.example.sana_visprog.dto.User.RegisterRequest
 import com.example.sana_visprog.model.AuthResponse
 import retrofit2.http.Body
 import retrofit2.http.POST
@@ -8,4 +9,7 @@ import retrofit2.http.POST
 interface AuthService {
     @POST("login")
     suspend fun login(@Body request: LoginRequest): AuthResponse
+
+    @POST("register")
+    suspend fun register(@Body request: RegisterRequest): AuthResponse
 }
