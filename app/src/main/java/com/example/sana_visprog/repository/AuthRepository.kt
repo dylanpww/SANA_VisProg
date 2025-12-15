17dba74ef8e0428e496f5c9715c28a97457cf05d
ClarrenceAdriano <cadriano@student.ciputra.ac.id>
2025-12-15 21:07:08 +0700

    Register Repo

--- app/src/main/java/com/example/sana_visprog/repository/AuthRepository.kt
+++ app/src/main/java/com/example/sana_visprog/repository/AuthRepository.kt
@@ -1,6 +1,7 @@
 package com.example.sana_visprog.repository
 
 import com.example.sana_visprog.dto.User.LoginRequest
+import com.example.sana_visprog.dto.User.RegisterRequest
 import com.example.sana_visprog.model.AuthResponse
 import com.example.sana_visprog.service.AuthService
 
@@ -9,5 +10,10 @@ class AuthRepository(private val apiService: AuthService) {
     suspend fun loginUser(email: String, password: String): AuthResponse {
         val request = LoginRequest(email, password)
         return apiService.login(request)
+    }
+
+    suspend fun registerUser(username: String, email: String, password: String): AuthResponse {
+        val request = RegisterRequest(username, email, password)
+        return apiService.register(request)
     }
 }
