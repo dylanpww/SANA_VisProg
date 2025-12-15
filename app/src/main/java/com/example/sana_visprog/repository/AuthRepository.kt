b288feefcad69ed7851b3f0e1d5f2600f64b302f
ClarrenceAdriano <cadriano@student.ciputra.ac.id>
2025-12-15 19:59:59 +0700

    repository changes

--- app/src/main/java/com/example/sana_visprog/repository/AuthRepository.kt
+++ app/src/main/java/com/example/sana_visprog/repository/AuthRepository.kt
@@ -1,8 +1,13 @@
 package com.example.sana_visprog.repository
 
 import com.example.sana_visprog.dto.User.LoginRequest
+import com.example.sana_visprog.model.AuthResponse
 import com.example.sana_visprog.service.AuthService
 
-class AuthRepository(private val api: AuthService) {
-    suspend fun loginUser(req: LoginRequest) = api.login(req)
+class AuthRepository(private val apiService: AuthService) {
+
+    suspend fun loginUser(email: String, password: String): AuthResponse {
+        val request = LoginRequest(email, password)
+        return apiService.login(request)
+    }
 }
