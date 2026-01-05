eeaa50cac837badbc0b6953d8fe929a13a2886c1
ClarrenceAdriano <cadriano@student.ciputra.ac.id>
2026-01-05 12:10:30 +0700

    UserPreference and Repository

--- app/src/main/java/com/example/sana_visprog/repository/AuthRepository.kt
+++ app/src/main/java/com/example/sana_visprog/repository/AuthRepository.kt
@@ -4,12 +4,30 @@ import com.example.sana_visprog.dto.User.LoginRequest
 import com.example.sana_visprog.dto.User.RegisterRequest
 import com.example.sana_visprog.model.AuthResponse
 import com.example.sana_visprog.service.AuthService
+import com.example.sana_visprog.utils.JwtUtils
 
-class AuthRepository(private val apiService: AuthService) {
+class AuthRepository(
+    private val apiService: AuthService,
+    private val userPreferences: UserPreferences
+) {
 
     suspend fun loginUser(email: String, password: String): AuthResponse {
         val request = LoginRequest(email, password)
-        return apiService.login(request)
+        val response = apiService.login(request)
+
+        if (response.data?.token != null) {
+            val token = response.data.token
+
+            var username = JwtUtils.decodeUsername(token)
+
+            if (username.isEmpty()) {
+                username = "SANA User"
+            }
+
+            userPreferences.saveSession(token, username)
+        }
+
+        return response
     }
 
     suspend fun registerUser(username: String, email: String, password: String): AuthResponse {
@@ -17,5 +35,7 @@ class AuthRepository(private val apiService: AuthService) {
         return apiService.register(request)
     }
 
-    companion object
+    suspend fun logout() {
+        userPreferences.logout()
+    }
 }
--- app/src/main/java/com/example/sana_visprog/repository/UserPreference.kt
+++ app/src/main/java/com/example/sana_visprog/repository/UserPreference.kt
@@ -1,6 +1,5 @@
 package com.example.sana_visprog.repository
 
-
 import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
@@ -10,20 +9,22 @@ import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
-
 val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "user_prefs")
 
 class UserPreferences(private val context: Context) {
     private val TOKEN_KEY = stringPreferencesKey("auth_token")
+    private val USER_NAME_KEY = stringPreferencesKey("user_name")
 
     val authToken: Flow<String?> = context.dataStore.data
-        .map { preferences ->
-            preferences[TOKEN_KEY]
-        }
+        .map { preferences -> preferences[TOKEN_KEY] }
 
-    suspend fun saveToken(token: String) {
+    val userName: Flow<String?> = context.dataStore.data
+        .map { preferences -> preferences[USER_NAME_KEY] }
+
+    suspend fun saveSession(token: String, name: String) {
         context.dataStore.edit { preferences ->
             preferences[TOKEN_KEY] = token
+            preferences[USER_NAME_KEY] = name
         }
     }
 
